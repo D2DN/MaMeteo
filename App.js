eb73a1d802ea490ef908b7b84371471b1fedf3fc
Dorian de Nicola <denicola.dorian@gmail.com>
2018-01-27 22:10:39 -0500

    Refactor file App.js

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
+import {StyleSheet, View, StatusBar} from 'react-native';
 import About from './components/About'
 import Search from './components/Search'
 import {TabNavigator} from 'react-navigation'
 
+// Define the tabBar navigation
 const Tabs = TabNavigator({
-  Search : { screen: Search},
-  About : { screen: About},
-},
-{ 
+  Search: {
+    screen: Search
+  },
+  About: {
+    screen: About
+  }
+}, {
   tabBarPosition: 'bottom',
   tabBarOptions: {
     showIcon: true,
     showLabel: false,
     indicatorStyle: {
       height: 2,
-      backgroundColor: 'white',
+      backgroundColor: 'white'
     },
     style: {
       backgroundColor: 'grey',
       borderTopWidth: 1,
-      borderColor: 'grey',
-    },
-  },
+      borderColor: 'grey'
+    }
+  }
 })
 
 export default class App extends React.Component {
@@ -39,6 +43,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 40, //TODO ==> For iphone X delete this line and verified if OK
-  },
+  }
 });
-
